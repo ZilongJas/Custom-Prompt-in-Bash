3307ad01d6d1df4f27fdbb1c40c7efa9d095212c
Zilong <165133185+ZilongJas@users.noreply.github.com>
2024-08-25 23:31:03 -0700

    Update custom_prompt.sh

--- custom_prompt.sh
+++ custom_prompt.sh
@@ -1,4 +1,6 @@
 PS1="\[$(tput setaf 2)\]☆ \[$(tput setaf 2)$(tput bold)\]\W \[$(tput setaf 7)\](\@)\[$(tput sgr0)\]\$ "
 
-echo "Custom prompt script loaded!"
+## Customize your welcome message here by uncommenting
+## or use cowsay for a more graphic welcoming message
 
+# echo "Your welcome message" 
